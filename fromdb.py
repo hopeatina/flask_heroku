984c955c962ee801d3e55155e505a96d56fa85f1
hopeatina <hopeatina@gmail.com>
2016-07-27 16:39:06 -0500

    matches, options and BACKEND left in connect hide controls, fix arrows

--- fromdb.py
+++ fromdb.py
@@ -13,9 +13,9 @@ gdb = GraphDatabase(os.environ.get("GRAPHENEDB_URL"))
 
 @from_api.route('/api/getexploredata', methods=['GET', 'POST'])
 def getexploredata():
-    nodequery = "MATCH (nodes)<-[r]- (user:User {value:" + request.data + "}) RETURN nodes"
-    nodeoriginquery = "MATCH (nodes)<-[r]- (user:User {value:" + request.data + "}) RETURN user"
-    relquery = "MATCH (nodes)<-[r]- (user:User {value:" + request.data + "}) RETURN r "
+    nodequery = "MATCH (nodes)-[r]-> (user:User {value:" + request.data + "}) RETURN nodes"
+    nodeoriginquery = "MATCH (nodes)-[r]-> (user:User {value:" + request.data + "}) RETURN user"
+    relquery = "MATCH (nodes)-[r]-> (user:User {value:" + request.data + "}) RETURN r "
     # relquery = "start n=node(*) MATCH (
     # result = gdb.query(q=query,data_contents=True)
     print("Got explorer data", request.data, nodequery, nodeoriginquery, relquery)
